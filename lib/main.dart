e182810eec6bf3877db7c793f71be3cbac384959
Michael Mogenson <michael.mogenson@gmail.com>
2021-09-19 18:44:56 -0400

    1 monkey + 1 macbook fixed this layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,46 +8,42 @@ class MyApp extends StatelessWidget {
 
   Widget frame() {
     return const Center(
-      child: Image(
-        image: AssetImage('images/Providence-Black-Frame.png'),
-        fit: BoxFit.fill,
+      child: SizedBox.expand(
+        child: Image(
+          image: AssetImage('images/Providence-Black-Frame.png'),
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
 
   Widget slideshow() {
-    return SizedBox.expand(
-      child: FractionallySizedBox(
-        // we squish the images to fit
-        widthFactor: 0.9,
-        heightFactor: 0.9,
-        alignment: FractionalOffset.center,
-        child: ImageSlideshow(
-          width: double.infinity,
-          height: double.infinity,
-          autoPlayInterval: 5000,
-          isLoop: true,
-          children: [
-            Image(
-              image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/D%C3%BClmen%2C_Hausd%C3%BClmen%2C_Heubach_--_2019_--_3260.jpg/1280px-D%C3%BClmen%2C_Hausd%C3%BClmen%2C_Heubach_--_2019_--_3260.jpg',
-              ),
-              fit: BoxFit.fill,
+    return Center(
+      child: ImageSlideshow(
+        width: double.infinity,
+        height: double.infinity,
+        autoPlayInterval: 5000,
+        isLoop: true,
+        children: [
+          Image(
+            image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/D%C3%BClmen%2C_Hausd%C3%BClmen%2C_Heubach_--_2019_--_3260.jpg/1280px-D%C3%BClmen%2C_Hausd%C3%BClmen%2C_Heubach_--_2019_--_3260.jpg',
             ),
-            Image(
-              image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Landscape_of_Shimla_%2C_Himachal_Pradesh.jpg/1280px-Landscape_of_Shimla_%2C_Himachal_Pradesh.jpg',
-              ),
-              fit: BoxFit.fill,
+            fit: BoxFit.fill,
+          ),
+          Image(
+            image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Landscape_of_Shimla_%2C_Himachal_Pradesh.jpg/1280px-Landscape_of_Shimla_%2C_Himachal_Pradesh.jpg',
             ),
-            Image(
-              image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_8069.jpg/1280px-D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_8069.jpg',
-              ),
-              fit: BoxFit.fill,
+            fit: BoxFit.fill,
+          ),
+          Image(
+            image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_8069.jpg/1280px-D%C3%BClmen%2C_Umland%2C_Sonnenaufgang_--_2012_--_8069.jpg',
             ),
-          ],
-        ),
+            fit: BoxFit.fill,
+          ),
+        ],
       ),
     );
   }
@@ -57,16 +53,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Center(
-        child: Container(
-          width: 1600,
-          height: 1000,
-          //color: Colors.amber[600],
-          child: Stack(
-            children: <Widget>[
-              slideshow(),
-              frame(),
-            ],
-          ),
+        child: Stack(
+          children: <Widget>[
+            slideshow(),
+            frame(),
+          ],
         ),
       ),
     );
